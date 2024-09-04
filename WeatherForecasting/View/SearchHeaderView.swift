f7e82bdea94620047e7f5ebc97c142c6aa734376
PurpleCactus9 <purplecactus900@gmail.com>
2024-09-04 10:09:03 -0500

    SearchHeaderView is done

---
+++ WeatherForecasting/View/SearchHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchHeaderView.swift
+//  WeatherForecasting
+//
+//  Created by AM Student on 9/4/24.
+//
+
+import SwiftUI
+
+struct SearchHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchHeaderView()
+}
