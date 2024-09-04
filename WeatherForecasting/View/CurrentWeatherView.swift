eab0f689badb80ef9d458c727c0d356d44a8a7d1
PurpleCactus9 <purplecactus900@gmail.com>
2024-09-04 10:46:29 -0500

    Working on CurrentWeatherView

---
+++ WeatherForecasting/View/CurrentWeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeatherView.swift
+//  WeatherForecasting
+//
+//  Created by AM Student on 9/4/24.
+//
+
+import SwiftUI
+
+struct CurrentWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentWeatherView()
+}
