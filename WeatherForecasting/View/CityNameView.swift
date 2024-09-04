1bcd74ee6282ae68c764453232eae0185a9d4e7d
PurpleCactus9 <purplecactus900@gmail.com>
2024-09-04 10:19:53 -0500

    CityNameView is done

---
+++ WeatherForecasting/View/CityNameView.swift
@@ -0,0 +1,18 @@
+//
+//  CityNameView.swift
+//  WeatherForecasting
+//
+//  Created by AM Student on 9/4/24.
+//
+
+import SwiftUI
+
+struct CityNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CityNameView()
+}
